1edd8828683a559b7ee636a08bfefd7a76df7491
pnikhileshreddy-2620 <pnikhileshreddy2620@gmail.com>
2025-03-19 05:49:30 +0000

    script is working

--- aws_resource_list.sh
+++ aws_resource_list.sh
@@ -2,54 +2,31 @@
 
 ###############################################################################
 # Author: Nikhilesh
-#version :v1
-#
-# Script to automate the process of listing all the resources in an AWS account
-#
-# Below are the services that are supported by this script:
-# 1. EC2
-# 2. RDS
-# 3. S3
-# 4. CloudFront
-# 5. VPC
-# 6. IAM
-# 7. Route53
-# 8. CloudWatch
-# 9. CloudFormation
-# 10. Lambda
-# 11. SNS
-# 12. SQS
-# 13. DynamoDB
-# 14. VPC
-# 15. EBS
-#
-# The script will prompt the user to enter the AWS region and the service for which the resources need to be listed.
-#
-# Usage: ./scriptname.sh  <aws_region> <aws_service>
-# permission for script depends upon req
-# Example: ./aws_resource_list.sh us-east-1 ec2
-#############################################################################
+
+# Script to list AWS resources in a given region
+###############################################################################
 
 # Check if the required number of arguments are passed
 if [ $# -ne 2 ]; then
-    echo "Usage: ./aws_resource_list.sh  <aws_region> <aws_service>"
-    echo "Example: ./aws_resource_list.sh us-east-1 ec2"
+    echo "Usage: $0 <aws_region> <aws_service>"
+    echo "Example: $0 us-east-1 ec2"
     exit 1
 fi
 
-# Assign the arguments to variables and convert the service to lowercase
+# Assign arguments to variables
 aws_region=$1
 aws_service=$2
 
-# Check if the AWS CLI is installed
+# Check if AWS CLI is installed
 if ! command -v aws &> /dev/null; then
-    echo "AWS CLI is not installed. Please install the AWS CLI and try again."
+    echo "AWS CLI is not installed. Please install it and try again."
     exit 1
 fi
 
-# Check if the AWS CLI is configured
-if [ ! -d ~/.aws ]; then
-    echo "AWS CLI is not configured. Please configure the AWS CLI and try again."
+# Check if AWS CLI is configured
+aws sts get-caller-identity &> /dev/null
+if [ $? -ne 0 ]; then
+    echo "AWS CLI is not properly configured. Please run 'aws configure' and try again."
     exit 1
 fi
 
@@ -57,62 +34,63 @@ fi
 case $aws_service in
     ec2)
         echo "Listing EC2 Instances in $aws_region"
-        aws ec2 describe-instances --region $aws_region
+        aws ec2 describe-instances --region "$aws_region"
         ;;
     rds)
         echo "Listing RDS Instances in $aws_region"
-        aws rds describe-db-instances --region $aws_region
+        aws rds describe-db-instances --region "$aws_region"
         ;;
     s3)
-        echo "Listing S3 Buckets in $aws_region"
-        aws s3api list-buckets --region $aws_region
+        echo "Listing S3 Buckets"
+        aws s3api list-buckets
         ;;
     cloudfront)
-        echo "Listing CloudFront Distributions in $aws_region"
-        aws cloudfront list-distributions --region $aws_region
+        echo "Listing CloudFront Distributions"
+        aws cloudfront list-distributions
         ;;
     vpc)
         echo "Listing VPCs in $aws_region"
-        aws ec2 describe-vpcs --region $aws_region
+        aws ec2 describe-vpcs --region "$aws_region"
         ;;
     iam)
-        echo "Listing IAM Users in $aws_region"
-        aws iam list-users --region $aws_region
+        echo "Listing IAM Users"
+        aws iam list-users
         ;;
-    route5)
-        echo "Listing Route53 Hosted Zones in $aws_region"
-        aws route53 list-hosted-zones --region $aws_region
+    route53)
+        echo "Listing Route53 Hosted Zones"
+        aws route53 list-hosted-zones
         ;;
     cloudwatch)
         echo "Listing CloudWatch Alarms in $aws_region"
-        aws cloudwatch describe-alarms --region $aws_region
+        aws cloudwatch describe-alarms --region "$aws_region"
         ;;
     cloudformation)
         echo "Listing CloudFormation Stacks in $aws_region"
-        aws cloudformation describe-stacks --region $aws_region
+        aws cloudformation describe-stacks --region "$aws_region"
         ;;
     lambda)
         echo "Listing Lambda Functions in $aws_region"
-        aws lambda list-functions --region $aws_region
+        aws lambda list-functions --region "$aws_region"
         ;;
     sns)
         echo "Listing SNS Topics in $aws_region"
-        aws sns list-topics --region $aws_region
+        aws sns list-topics --region "$aws_region"
         ;;
     sqs)
         echo "Listing SQS Queues in $aws_region"
-        aws sqs list-queues --region $aws_region
+        aws sqs list-queues --region "$aws_region"
         ;;
     dynamodb)
         echo "Listing DynamoDB Tables in $aws_region"
-        aws dynamodb list-tables --region $aws_region
+        aws dynamodb list-tables --region "$aws_region"
         ;;
     ebs)
         echo "Listing EBS Volumes in $aws_region"
-        aws ec2 describe-volumes --region $aws_region
+        aws ec2 describe-volumes --region "$aws_region"
         ;;
     *)
-        echo "Invalid service. Please enter a valid service."
+        echo "Invalid service: $aws_service. Please enter a valid AWS service."
         exit 1
         ;;
 esac
+
